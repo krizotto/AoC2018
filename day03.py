e41da8aa35228560696c13140590ee92a18cd7f6
Krzysztof Zawadka <kzawadka@gmail.com>
2022-11-16 19:27:46 +0100

    Day 3 small optimizations

--- day03.py
+++ day03.py
@@ -1,6 +1,7 @@
 from collections import Counter
 import re
 
+
 def process(claim: str):
     returnlist = []
     claim_id, x, y, range_x, range_y = list(
@@ -17,16 +18,17 @@ input = [x.strip() for x in open("data/day03.txt", "r")]
 
 # PART 1
 points = []
+points_with_ids = {}
 for claim in input:
     curr_points, claim_id = process(claim)
     points.extend(curr_points)
+    points_with_ids[claim_id] = curr_points
 points_more_than_one = [p for p, cnt in dict(Counter(points)).items() if cnt > 1]
 
 print(len(points_more_than_one))
 
 # PART 2
-for claim in input:
-    curr_points, claim_id = process(claim)
-    if curr_points.isdisjoint(points_more_than_one):
+for claim_id, pts in points_with_ids.items():
+    if pts.isdisjoint(points_more_than_one):
         print(claim_id)
         break
