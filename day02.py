541afed541cbea42f2171232bf09297622765efc
Krzysztof Zawadka <kzawadka@gmail.com>
2022-11-16 17:23:45 +0100

    Day 2 solution

--- day02.py
+++ day02.py
@@ -1,11 +1,9 @@
 # PART 1
-
-
 def check_freq(x):
     return {c: x.count(c) for c in set(x)}
 
 
-day2_input = [x for x in open("data/day02test.txt", "r")]
+day2_input = [x.strip() for x in open("data/day02.txt", "r")]
 
 count_two = 0
 count_three = 0
@@ -21,19 +19,21 @@ print(count_two * count_three)
 # PART 2
 first_box = ""
 second_box = ""
-current_distinguish = 1000
-for i in range(len(day2_input)):
-    for j in range(i + 1, len(day2_input)):
-        temp_distinguish = 0
-        for it in range(len(day2_input[i])):
-            temp_distinguish += day2_input[i][it] == day2_input[j][it]
-        if temp_distinguish < current_distinguish:
-            current_distinguish = temp_distinguish
+current_common = 0
+box_count = len(day2_input)
+box_size = len(day2_input[0])
+for i in range(box_count):
+    for j in range(i + 1, box_count):
+        common_count = 0
+        for it in range(box_size):
+            common_count += day2_input[i][it] == day2_input[j][it]
+        if common_count > current_common:
+            current_common = common_count
             first_box = day2_input[i]
             second_box = day2_input[j]
 
 answer = ""
-for it in range(len(first_box)):
+for it in range(box_size):
     if first_box[it] == second_box[it]:
         answer += first_box[it]
 
