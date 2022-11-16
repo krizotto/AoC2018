072055eadb3237260cba7cebfe1fcc269eacea50
Krzysztof Zawadka <kzawadka@gmail.com>
2022-11-16 18:53:34 +0100

    Optimized day 3

--- day03.py
+++ day03.py
@@ -1,43 +1,32 @@
 from collections import Counter
 import re
 
-
 def process(claim: str):
-    if claim == "":
-        return []
     returnlist = []
     claim_id, x, y, range_x, range_y = list(
         filter(None, re.split("  | |@|,|:|x", claim))
     )
-    x = int(x)
-    y = int(y)
-    range_x = int(range_x)
-    range_y = int(range_y)
+    x, y, range_x, range_y = int(x), int(y), int(range_x), int(range_y)
     for i in range(range_x):
         for j in range(range_y):
             returnlist.append((x + i, y + j))
-    return returnlist, claim_id
+    return set(returnlist), claim_id
 
 
-points = []
 input = [x.strip() for x in open("data/day03.txt", "r")]
+
+# PART 1
+points = []
 for claim in input:
-    curr_points, claim_ids = process(claim)
+    curr_points, claim_id = process(claim)
     points.extend(curr_points)
-counter_dict = dict(Counter(points)).items()
-points_more_than_one = [p for p, cnt in counter_dict if cnt > 1]
+points_more_than_one = [p for p, cnt in dict(Counter(points)).items() if cnt > 1]
 
 print(len(points_more_than_one))
-# freq = check_freq(points)
 
 # PART 2
 for claim in input:
-    curr_points, claim_ids = process(claim)
-    distinct = True
-    for p in curr_points:
-        if p in points_more_than_one:
-            distinct = False
-            break
-    if distinct:
-        print(claim_ids)
+    curr_points, claim_id = process(claim)
+    if curr_points.isdisjoint(points_more_than_one):
+        print(claim_id)
         break
